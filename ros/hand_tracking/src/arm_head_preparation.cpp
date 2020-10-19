f6a0966990c6bd2b1d2800adb71a5effa585c0b2
Author Sli <sli@informatik.uni-hamburg.de>
2020-10-19 09:13:17 +0000

    arm head preparation bug fixed

--- ros/hand_tracking/src/arm_head_preparation.cpp
+++ ros/hand_tracking/src/arm_head_preparation.cpp
@@ -1,14 +1,15 @@
 #include <ros/ros.h>
+#include <pr2_mechanism_msgs/LoadController.h>
 #include <moveit/move_group_interface/move_group_interface.h>
-#include <controller_manager_msgs/SwitchController.h>
+#include <pr2_mechanism_msgs/SwitchController.h>
 
 
 void velocity2trajectory_controllers(ros::NodeHandle &nh, bool left_teleop, bool right_teleop) {
 
-    ros::ServiceClient switchClient = nh.serviceClient<controller_manager_msgs::SwitchController>(
+    ros::ServiceClient switchClient = nh.serviceClient<pr2_mechanism_msgs::SwitchController>(
             "pr2_controller_manager/switch_controller");
 
-    controller_manager_msgs::SwitchController switchSrv;
+    pr2_mechanism_msgs::SwitchController switchSrv;
     if (left_teleop) {
         switchSrv.request.stop_controllers.push_back("l_arm_joint_group_velocity_controller");
         switchSrv.request.start_controllers.push_back("l_arm_controller");
@@ -23,18 +24,24 @@ void velocity2trajectory_controllers(ros::NodeHandle &nh, bool left_teleop, bool
 }
 
 void trajectory2velocity_controllers(ros::NodeHandle &nh, bool left_teleop, bool right_teleop) {
+    ros::ServiceClient loadClient = nh.serviceClient<pr2_mechanism_msgs::LoadController>(
+            "pr2_controller_manager/load_controller");
+    pr2_mechanism_msgs::LoadController loadSrv;
 
-    ros::ServiceClient switchClient = nh.serviceClient<controller_manager_msgs::SwitchController>(
+    ros::ServiceClient switchClient = nh.serviceClient<pr2_mechanism_msgs::SwitchController>(
             "pr2_controller_manager/switch_controller");
-
-    controller_manager_msgs::SwitchController switchSrv;
+    pr2_mechanism_msgs::SwitchController switchSrv;
     if (left_teleop) {
-        switchSrv.request.stop_controllers.push_back("l_arm_joint_group_velocity_controller");
-        switchSrv.request.start_controllers.push_back("l_arm_controller");
+        loadSrv.request.name = "l_arm_joint_group_velocity_controller";
+        loadClient.call(loadSrv);
+        switchSrv.request.start_controllers.push_back("l_arm_joint_group_velocity_controller");
+        switchSrv.request.stop_controllers.push_back("l_arm_controller");
     }
     if (right_teleop) {
-        switchSrv.request.stop_controllers.push_back("r_arm_joint_group_velocity_controller");
-        switchSrv.request.start_controllers.push_back("r_arm_controller");
+        loadSrv.request.name = "r_arm_joint_group_velocity_controller";
+        loadClient.call(loadSrv);
+        switchSrv.request.start_controllers.push_back("r_arm_joint_group_velocity_controller");
+        switchSrv.request.stop_controllers.push_back("r_arm_controller");
     }
 
     switchSrv.request.strictness = 2; //controller_manager_msgs::SwitchController::STRICT;
@@ -52,7 +59,7 @@ int main(int argc, char **argv)
     {
         std::cout << "------------------Choose one of the following action------------------" << std::endl;
         std::cout  << "Press 1: Head On please" << std::endl;
-        std::cout << "Press 2: Left arm pose is ready" << std::endl;
+        std::cout << "Press 2: Move left arm to starting pose please" << std::endl;
         std::cout << "Press 3: Left arm from trajectory control to velocity control" << std::endl;
         std::cout << "Press 4: Right arm from trajectory control to velocity control" << std::endl;
         std::cout << "Press 5: Left arm from velocity control to trajectory control" << std::endl;
@@ -68,7 +75,7 @@ int main(int argc, char **argv)
             std::vector<double> pr2_head_position{0.32, 0.51};
             head_mgi_->setJointValueTarget(pr2_head_position);
             head_mgi_->move();
-            std::cout << "Head On please" << std::endl;
+            std::cout << "Head On done" << std::endl;
             break;
         }
         case '2':{
@@ -76,23 +83,23 @@ int main(int argc, char **argv)
             std::vector<double> left_arm_start_position{0.0, 0.63, 0.2, -0.15, 0.0, -0.31, -0.0};
             left_arm_mgi_->setJointValueTarget(left_arm_start_position);
             left_arm_mgi_->move();
-            std::cout << "Left arm pose is ready" << std::endl;
+            std::cout << "Left arm pose is done" << std::endl;
             break;}
         case '3':{
-            velocity2trajectory_controllers(nh, "true", "false");
-            std::cout << "Left arm from trajectory control to velocity control" << std::endl;
+            trajectory2velocity_controllers(nh, true, false);
+            std::cout << "Done: left arm from trajectory control to velocity control" << std::endl;
             break;}
         case '4':{
-            velocity2trajectory_controllers(nh, "false", "true");
-            std::cout << "Right arm from trajectory control to velocity control" << std::endl;
+            trajectory2velocity_controllers(nh, false, true);
+            std::cout << "Done: right arm from trajectory control to velocity control" << std::endl;
             break;}
         case '5':{
-            trajectory2velocity_controllers(nh, "true", "false");
-            std::cout << "Left arm from velocity control to trajectory control" << std::endl;
+            velocity2trajectory_controllers(nh, true, false);
+            std::cout << "Done: left arm from velocity control to trajectory control" << std::endl;
             break;}
         case '6':{
-            trajectory2velocity_controllers(nh, "false", "true");
-            std::cout << "Right arm from velocity control to trajectory control" << std::endl;
+            velocity2trajectory_controllers(nh, false, true);
+            std::cout << "Done: right arm from velocity control to trajectory control" << std::endl;
             break;}
         case 'q' :{
             ros::shutdown();
